39790c34a94e3b03c3f09ef2e8f3a68c697a507f
villa <bryan.m.villa.c@gmail.com>
2024-12-19 12:46:44 -0500

    Solucion

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,25 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        String cadena="";
+
+        for (int fila = 0; fila < estudiantes.length; fila++) {
+            for (int col = 0; col < estudiantes[fila].length; col++) {
+                char letra = estudiantes[fila][col].charAt(0);
+                
+                if (letra == 'S' || letra == 'P'|| letra =='T') {
+                    cadena = String.format("%s%s\n ", cadena,estudiantes[fila][col] );
+                }
+                
+            }
+        }
+        System.out.printf("%s",cadena);
+    }
 }
