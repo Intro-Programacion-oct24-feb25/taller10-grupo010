72b97d63285d37855fa53afaee8a926a55e7f5de
villa <bryan.m.villa.c@gmail.com>
2024-12-19 12:33:45 -0500

    Solucion

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,41 @@
  */
 package paquete4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        double [][] datos=new double[2][2];
+        double valor;
+        int fueraRango=0;
+        
+        for (int fila = 0; fila < datos.length; fila++) {
+            for (int col = 0; col < datos[fila].length; col++) {
+                System.out.println("Ingrese el valor");
+                valor= entrada.nextDouble();
+                // < >
+                if (valor > 1000 && valor < 1999 ) {
+                    datos[fila][col]= valor;
+                } else {
+                    datos[fila][col]= 10.0;
+                    fueraRango=fueraRango+1;
+                }
+            }
+        }
+        
+        for (int fila = 0; fila < datos.length; fila++) {
+            for (int col = 0; col < datos[fila].length; col++) {
+                System.out.println(datos[fila][col]);
+                
+            }
+        }
+        System.out.printf(" exiten %d datos fura del rango",fueraRango);
+        
+    }
 }
